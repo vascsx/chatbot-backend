811f87294788a59ba276c48be7d7844bd5a401ec
vascsx <vasczn@gmail.com>
2024-03-13 22:42:19 -0300

    feat: add string treatment before matching keyword

--- src/main/java/com/kipper/museumchatbot/services/FaqService.java
+++ src/main/java/com/kipper/museumchatbot/services/FaqService.java
@@ -16,10 +16,11 @@ public class FaqService {
     public String getAnswer(String question) {
 
         String[] words = question.toLowerCase().split("\\s+");
-        List wordsList = Arrays.asList(words);
+        List wordsList = Arrays.asList(words).stream().map(String::toLowerCase).toList();
+        System.out.println(wordsList);
         for (FaqAnswer entry : faqAnswers.getAnswers()) {
             for (String keyword : entry.getKeywords()) {
-                if ( wordsList.contains(keyword)) {
+                if ( wordsList.contains(keyword.toLowerCase())) {
                     return entry.getAnswer();
                 }
             }
